cf52ee183e0dc2c1713655755f4299d6abb85dff
silaspokemon <silasborcher@gmail.com>
2022-11-27 20:05:36 +0100

    first commit

---
+++ src/game.zig
@@ -0,0 +1,93 @@
+const std = @import("std");
+const rl = @import("raylib/raylib.zig");
+const levels = @import("levels.zig");
+const objects = @import("objects.zig");
+const level1 = @import("levels/1.zig");
+
+
+pub fn initMap(level: levels.levelData) objects.map {
+    var map: [180]objects.tile = undefined;
+    var iteration: u8 = 0;
+    for (level.grid) |row,rowIndex| {
+        for (row) |pos,posIndex| {
+            if (pos != 0) {
+                map[iteration] = objects.tile {
+                    .rect = rl.Rectangle {
+                        .x = @intToFloat(f32, posIndex*50),
+                        .y = @intToFloat(f32, rowIndex*50),
+                        .width = 50,
+                        .height = 50,
+                    },
+                    .type = pos, 
+                };
+                iteration += 1;
+            }
+        }
+    }
+    return objects.map {
+        .grid = map[0..iteration],
+        .spawnPos = level.spawnPos,
+        .id = level.id,
+    };
+}
+
+pub fn draw(map: objects.map, player: objects.player) void {
+    rl.ClearBackground(rl.RAYWHITE);
+    for (map.grid) |tile| {
+        rl.DrawRectangleRec(tile.rect, switch (tile.type) {
+            0=>rl.WHITE,
+            1=>rl.BLACK,
+            2=>rl.RED,
+            3=>rl.LIME,
+            4=>rl.YELLOW,
+            5=>rl.GREEN,
+            6=>rl.PURPLE,
+            7=>rl.VIOLET,
+            8=>rl.LIGHTGRAY,
+            9=>rl.GOLD,
+            else=>rl.BLACK
+        });
+    }
+    rl.DrawRectangleRec(player.rect, rl.RED);
+}
+
+pub fn bounds(player: *objects.player, map: *objects.map, state: *u8) void {
+    if (player.rect.x > 870) {
+        const transitionTo = level1.transition(map.id);
+        if (transitionTo[2].id != 255) {
+            player.rect.x = 0;
+            map.* = initMap(transitionTo[2]);
+        }
+        else player.rect.x = 870;
+    }
+    else if (player.rect.x < 0) {
+        const transitionTo = level1.transition(map.id);
+        if (transitionTo[0].id != 255) {
+            map.* = initMap(transitionTo[0]);
+            player.rect.x = 870;
+        }
+        else {
+            player.rect.x = 0;
+        }
+    }
+    if (player.rect.y < 0) {
+        const transitionTo = level1.transition(map.id);
+        if (transitionTo[1].id != 255) {
+            player.rect.y = 440;
+            map.* = initMap(transitionTo[1]);
+        }
+        else if (player.gravity < 0) {
+            player.die(state);
+        }
+    }
+    if (player.rect.y > 440) {
+        const transitionTo = level1.transition(map.id);
+        if (transitionTo[3].id != 255) {
+            player.rect.y = 0;
+            map.* = initMap(transitionTo[3]);
+        }
+        else if (player.gravity > 0) {
+            player.die(state);
+        }
+    } 
+}
---
+++ src/main.zig
@@ -0,0 +1,103 @@
+const std = @import("std");
+const rl = @import("raylib/raylib.zig");
+const levels = @import("levels.zig");
+const objects = @import("objects.zig");
+const game = @import("game.zig");
+const level1 = @import("levels/1.zig");
+
+
+pub fn main() void {
+    const screenWidth: u16 = 900;
+    const screenHeight: u16 = 500;
+
+    rl.InitWindow(screenWidth,screenHeight, "Tilemap Loading");
+    rl.SetTargetFPS(60);
+    rl.SetExitKey(.KEY_NULL);
+
+    var exitGame: bool = false;
+    var map: objects.map = undefined;
+    var player: objects.player = undefined;
+    var state: u8 = 0;
+
+    while (!exitGame and !rl.WindowShouldClose()) {
+        rl.BeginDrawing();
+        defer rl.EndDrawing();
+        rl.DrawFPS(0, 0);
+        switch (state) {
+            0=>{
+                rl.ClearBackground(rl.RAYWHITE);
+                var playButton = objects.button {.rect = rl.Rectangle {.x = 100, .y = 200, .width = 700, .height = 100}, .hovered = false, .text = "Play Game", .fontsize = 50 };
+                var exitButton = objects.button {.rect = rl.Rectangle {.x = 100, .y = 325, .width = 700, .height = 100}, .hovered = false, .text = "Exit Game", .fontsize = 50 };
+                exitButton.render();
+                playButton.render();
+                if (playButton.getClicked()) {
+                    map = game.initMap(level1.x1y1);
+                    player = objects.player.init(map.spawnPos.x, map.spawnPos.y);
+                    state = 1;
+                }
+                if (exitButton.getClicked()) {
+                    exitGame = true;
+                }
+            },
+            1=>{
+                if (rl.IsKeyPressed(.KEY_ESCAPE)) state = 3;
+                player.update(&map, &state);
+                game.bounds(&player,&map, &state);
+                game.draw(map, player);
+            },
+            2=>{
+                const backGroundBox = rl.Rectangle {.x = 200, .y = 50, .width = 500, .height = 300};
+                rl.DrawRectangleRec(backGroundBox, rl.LIGHTGRAY);
+                rl.DrawRectangleLinesEx(backGroundBox, 5,  rl.GRAY);
+                rl.DrawText("You Died", screenWidth / 2 - @divTrunc(rl.MeasureText("You Died", 75), 2), 100, 75, rl.BLACK);
+                var titleButton = objects.button {
+                    .rect = rl.Rectangle {.x = 225, .y = 200, .width = 200, .height = 100},
+                    .text = "Title Screen",
+                    .fontsize = 20,
+                };
+                var respawnButton = objects.button {
+                    .rect = rl.Rectangle {.x = 475, .y = 200, .width = 200, .height = 100},
+                    .text = "Respawn",
+                    .fontsize = 20,
+                };
+                respawnButton.render();
+                titleButton.render();
+                if (respawnButton.getClicked()) {
+                    player.rect.x = map.spawnPos.x;
+                    player.rect.y = map.spawnPos.y;
+                    state = 1;
+                }
+                if (titleButton.getClicked()) {
+                    state = 0;
+                }
+            },
+            3=>{
+                const backGroundBox = rl.Rectangle {.x = 200, .y = 50, .width = 500, .height = 300};
+                rl.DrawRectangleRec(backGroundBox, rl.LIGHTGRAY);
+                rl.DrawRectangleLinesEx(backGroundBox, 5,  rl.GRAY);
+                rl.DrawText("Paused", screenWidth / 2 - @divTrunc(rl.MeasureText("Paused", 75), 2), 100, 75, rl.BLACK);
+                var titleButton = objects.button {
+                    .rect = rl.Rectangle {.x = 225, .y = 200, .width = 200, .height = 100},
+                    .text = "Title Screen",
+                    .fontsize = 20,
+                };
+                var unpauseButton = objects.button {
+                    .rect = rl.Rectangle {.x = 475, .y = 200, .width = 200, .height = 100},
+                    .text = "Resume",
+                    .fontsize = 20,
+                };
+                unpauseButton.render();
+                titleButton.render();
+                if (unpauseButton.getClicked()) {
+                    state = 1;
+                }
+                if (titleButton.getClicked()) {
+                    state = 0;
+                }
+                if (rl.IsKeyPressed(.KEY_ESCAPE)) state = 1;
+            },
+            else=>unreachable,
+        }
+    }
+}
+
