f619ac5580871bbb6341e6f95f2ebee2117ac318
silaspokemon <silasborcher@gmail.com>
2022-11-29 13:37:57 +0100

    Replaced if chain with a switch statement (else was disposed of poorly though)

--- src/objects.zig
+++ src/objects.zig
@@ -9,39 +9,33 @@ pub const button = struct {
     text: [*:0]const u8,
     fontsize: i32,
     pub fn checkHover(self: *button) bool {
-        if (rl.CheckCollisionPointRec(rl.Vector2 {.x = @intToFloat(f32, rl.GetMouseX()), .y = @intToFloat(f32, rl.GetMouseY())}, self.rect)) return true else return false; 
+        if (rl.CheckCollisionPointRec(rl.Vector2{ .x = @intToFloat(f32, rl.GetMouseX()), .y = @intToFloat(f32, rl.GetMouseY()) }, self.rect)) return true else return false;
     }
     pub fn getClicked(self: *button) bool {
         if (self.checkHover()) {
             self.hovered = true;
             if (rl.IsMouseButtonPressed(.MOUSE_BUTTON_LEFT) or rl.IsKeyPressed(.KEY_ENTER)) {
                 return true;
-            }
-            else {
+            } else {
                 return false;
-            } 
-        }
-        else {
+            }
+        } else {
             self.hovered = false;
             return false;
-        } 
+        }
     }
     pub fn render(self: *button) void {
         rl.DrawRectangleRec(self.rect, rl.GRAY);
         self.hovered = self.checkHover();
         rl.DrawRectangleLinesEx(self.rect, 5, switch (self.hovered) {
-            false=>rl.BLACK,
-            true=>rl.SKYBLUE,
+            false => rl.BLACK,
+            true => rl.SKYBLUE,
         });
-        rl.DrawText(self.text, @floatToInt(i32, self.rect.x) + @divTrunc(@floatToInt(i32, self.rect.width), 2) - @divTrunc(rl.MeasureText(self.text, self.fontsize), 2), @floatToInt(i32, self.rect.y) + @divTrunc(@floatToInt(i32, self.rect.height),2) - @divTrunc(self.fontsize, 2), self.fontsize, rl.BLACK);
+        rl.DrawText(self.text, @floatToInt(i32, self.rect.x) + @divTrunc(@floatToInt(i32, self.rect.width), 2) - @divTrunc(rl.MeasureText(self.text, self.fontsize), 2), @floatToInt(i32, self.rect.y) + @divTrunc(@floatToInt(i32, self.rect.height), 2) - @divTrunc(self.fontsize, 2), self.fontsize, rl.BLACK);
     }
 };
 
-pub const map = struct {
-    grid: []tile,
-    spawnPos: rl.Vector2,
-    id: u8 = undefined
-};
+pub const map = struct { grid: []tile, spawnPos: rl.Vector2, id: u8 = undefined };
 
 pub const tile = struct {
     rect: rl.Rectangle,
@@ -58,21 +52,21 @@ pub const player = struct {
     gravity: f32 = 1,
     pub fn init(x: f32, y: f32) player {
         return player{
-            .rect = rl.Rectangle {
+            .rect = rl.Rectangle{
                 .x = x,
                 .y = y,
                 .width = 30,
                 .height = 60,
             },
-            .velocity = rl.Vector2 {.x = 0, .y = 0},
+            .velocity = rl.Vector2{ .x = 0, .y = 0 },
             .grounded = false,
             .airJumped = false,
         };
     }
     pub fn colCheck(self: *player, obstacles: *map, x: f32, y: f32) tile {
-        var fallbackRect = tile {.rect = undefined, .type = 0};
-        for (obstacles.grid) |obstacle,index| {
-            if (rl.CheckCollisionRecs(obstacle.rect, rl.Rectangle {
+        var fallbackRect = tile{ .rect = undefined, .type = 0 };
+        for (obstacles.grid) |obstacle, index| {
+            if (rl.CheckCollisionRecs(obstacle.rect, rl.Rectangle{
                 .x = self.rect.x + x,
                 .y = self.rect.y + y,
                 .width = self.rect.width,
@@ -82,13 +76,11 @@ pub const player = struct {
                     self.jumpHeight = 13;
                     self.walkSpeed = 4;
                     return obstacles.grid[index];
-                }
-                else {
+                } else {
                     if (obstacle.type != 2) {
                         fallbackRect.rect = obstacle.rect;
                         fallbackRect.type = obstacle.type;
-                    }
-                    else if (fallbackRect.type == 0) {
+                    } else if (fallbackRect.type == 0) {
                         fallbackRect.rect = obstacle.rect;
                         fallbackRect.type = obstacle.type;
                     }
@@ -105,20 +97,17 @@ pub const player = struct {
     fn input(self: *player) void {
         if (rl.IsKeyPressed(.KEY_UP)) {
             if (self.grounded) {
-                self.velocity.y = -self.jumpHeight*self.gravity;
-            }
-            else if (!self.airJumped) {
-                self.velocity.y = -self.jumpHeight*self.gravity;
+                self.velocity.y = -self.jumpHeight * self.gravity;
+            } else if (!self.airJumped) {
+                self.velocity.y = -self.jumpHeight * self.gravity;
                 self.airJumped = true;
             }
         }
         if (rl.IsKeyDown(.KEY_RIGHT)) {
             self.velocity.x = self.walkSpeed;
-        }
-        else if (rl.IsKeyDown(.KEY_LEFT)) {
+        } else if (rl.IsKeyDown(.KEY_LEFT)) {
             self.velocity.x = -self.walkSpeed;
-        }
-        else {
+        } else {
             self.velocity.x = 0;
         }
     }
@@ -129,31 +118,32 @@ pub const player = struct {
             self.grounded = true;
             self.airJumped = false;
             tileInteract(self, lastCol.type, state);
-        }
-        else {
-            self.velocity.y +=self.gravity;
+        } else {
+            self.velocity.y += self.gravity;
             self.grounded = false;
         }
         lastCol = self.colCheck(obstacles, self.velocity.x, 0);
         if (!doesCollide(lastCol.type)) {
             self.rect.x += self.velocity.x;
             tileInteract(self, lastCol.type, state);
-        }
-        else {
-            if (self.velocity.x > 0) {self.rect.x = lastCol.rect.x - self.rect.width;}
-            else {self.rect.x = lastCol.rect.x + lastCol.rect.width;}
+        } else {
+            if (self.velocity.x > 0) {
+                self.rect.x = lastCol.rect.x - self.rect.width;
+            } else {
+                self.rect.x = lastCol.rect.x + lastCol.rect.width;
+            }
             self.velocity.x = 0;
         }
         lastCol = self.colCheck(obstacles, 0, self.velocity.y);
         if (!doesCollide(lastCol.type)) {
-            self.rect.y += self.velocity.y; 
+            self.rect.y += self.velocity.y;
             tileInteract(self, lastCol.type, state);
-        }
-        else {
+        } else {
             if (self.velocity.y > 0) {
                 self.rect.y = lastCol.rect.y - self.rect.height;
-            } 
-            else {self.rect.y = lastCol.rect.y + lastCol.rect.height;}
+            } else {
+                self.rect.y = lastCol.rect.y + lastCol.rect.height;
+            }
             self.velocity.y = 0;
         }
     }
@@ -164,42 +154,35 @@ pub const player = struct {
         self.gravity = 1;
     }
     fn tileInteract(self: *player, collideType: u8, state: *u8) void {
-        if (collideType==2) {
-            self.die(state);
-        }
-        if (collideType==3) {
-            self.jumpHeight = 20;
-        }
-        if (collideType==4) {
-            self.walkSpeed = 10;
-        }
-        if (collideType==5) {
-            self.jumpHeight = 20;
-            self.walkSpeed = 10;
-        }
-        if (collideType==6) {
-            self.velocity.y = -16;
-            self.airJumped = false;
-        }
-        if (collideType==7) {
-            self.gravity = -self.gravity;
-        }
-        if (collideType==8) {
-            self.airJumped = false;
+        switch (collideType) {
+            2 => self.die(state),
+            3 => self.jumpHeight = 20,
+            4 => self.walkSpeed = 10,
+            5 => {
+                self.jumpHeight = 20;
+                self.walkSpeed = 10;
+            },
+            6 => {
+                self.velocity.y = -16;
+                self.airJumped = false;
+            },
+            7 => self.gravity = -self.gravity,
+            8 => self.airJumped = false,
+            else => _ = 0,
         }
     }
     fn doesCollide(typic: u8) bool {
         return switch (typic) {
-            0=>false,
-            1=>true,
-            2=>false,
-            3=>true,
-            4=>true,
-            5=>true,
-            6=>true,
-            7=>true,
-            8=>false,
-            else=>false,
+            0 => false,
+            1 => true,
+            2 => false,
+            3 => true,
+            4 => true,
+            5 => true,
+            6 => true,
+            7 => true,
+            8 => false,
+            else => false,
         };
     }
 };
